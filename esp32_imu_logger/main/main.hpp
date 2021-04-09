a0889ca5126ab616ac8448e8943fc9893309fae4
DomFo <dominik.fohrmann@gmail.com>
2021-04-09 12:13:11 +0200

    added data visualization via udp live stream

--- esp32_imu_logger/main/main.hpp
+++ esp32_imu_logger/main/main.hpp
@@ -1,3 +1,5 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
 
 /* Bus configuration */
 
@@ -109,3 +111,4 @@ static int socket_add_ipv4_multicast_group(int sock, bool assign_source_if);
 
 static int create_multicast_ipv4_socket(void);
 
+#endif
