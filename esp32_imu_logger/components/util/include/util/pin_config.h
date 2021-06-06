8f92527db70d4ce20fbbedf9bbbf2b1d1eb07462
DomFo <dominik.fohrmann@gmail.com>
2021-06-06 03:10:25 +0200

    adapted pin config file to current hardware revision

--- esp32_imu_logger/components/util/include/util/pin_config.h
+++ esp32_imu_logger/components/util/include/util/pin_config.h
@@ -7,6 +7,7 @@
 
 #define PIN_NUM_IMU_SPI_CS          GPIO_NUM_5
 
+#define PIN_NUM_DEBUG_PIN          GPIO_NUM_13
 #define PIN_NUM_SDMMC_CLK          GPIO_NUM_14
 #define PIN_NUM_SDMMC_CMD          GPIO_NUM_15
 
@@ -14,12 +15,13 @@
 #define PIN_NUM_IMU_SPI_CLK        GPIO_NUM_18
 #define PIN_NUM_IMU_SPI_MISO       GPIO_NUM_19
 
-#define PIN_NUM_DEBUG_PIN          GPIO_NUM_22
 #define PIN_NUM_IMU_SPI_MOSI       GPIO_NUM_23
 
+#define PIN_NUM_SYNC_PIN           GPIO_NUM_26
+
 #define PIN_NUM_STATUS_LED         GPIO_NUM_32
-// #define PIN_NUM_ADC_VBAT           GPIO_NUM_33
-#define PIN_NUM_ADC_VBAT           GPIO_NUM_36
+#define PIN_NUM_ADC_VBAT           GPIO_NUM_33
+// #define PIN_NUM_ADC_VBAT           GPIO_NUM_36  // DEV BOARD RevB
 
 
 
